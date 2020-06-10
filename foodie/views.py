af21675287203936f73309a8fc60f5f26f96540d
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-11 01:58:10 +0900

    テンプレート追加

--- foodie/views.py
+++ foodie/views.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 GNAVI_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
 GNAVI_KEY = "c217fca9b867dc74d2e68d3ec67af7f8"
 
-class IndexView(TempplateView):
-    template_name = 'foodie.index.html'
+class IndexView(TemplateView):
+    template_name = 'foodie/index.html'
