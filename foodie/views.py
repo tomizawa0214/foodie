2038aab26a1f50fa4046a2b58026c87b7bd85e38
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-11 01:01:21 +0900

    View追加

--- foodie/views.py
+++ foodie/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+GNAVI_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+GNAVI_KEY = "c217fca9b867dc74d2e68d3ec67af7f8"
+
+class IndexView(TempplateView):
+    template_name = 'foodie.index.html'
