8e00db1bb9a5aabf33b697f13cbaa679287b3e5a
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-11 00:52:59 +0900

    管理画面作成

--- foodie/admin.py
+++ foodie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Pref
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_1', 'name')
+    list_display_links = ('category_1',)
+    list_editable = ('name',)
+
+@admin.register(Pref)
+class PrefAdmin(admin.ModelAdmin):
+    list_display = ('pref', 'name')
+    list_display_links = ('pref',)
+    list_editable = ('name',)
