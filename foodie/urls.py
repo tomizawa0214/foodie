db840e4905f4346acc7d9b3bd0b9b61a8660ff97
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-12 14:51:13 +0900

    詳細ページurls追加

--- foodie/urls.py
+++ foodie/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('search/', views.Search, name='search'),
+    path('shop_info/<str:restid>/', views.ShopInfo, name='shop_info'),
 ]
