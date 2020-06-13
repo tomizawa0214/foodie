cfa948f02cb4b8165853e43bb435eae1e9464b92
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-06-13 16:12:12 +0900

    口コミ機能models追加

--- foodie/models.py
+++ foodie/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     category_1 = models.CharField("大業態コード", max_length=10, blank=False)
@@ -13,3 +14,31 @@ class Pref(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Review(models.Model):
+    SCORE_CHOICES= [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+
+    shop_id = models.CharField('レストランID', max_length=10, blank=False)
+    shop_name = models.CharField('レストラン名', max_length=200, blank=False)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    comment = models.TextField(verbose_name='口コミ', blank=False)
+    score = models.PositiveSmallIntegerField(verbose_name='スコア', choices=SCORE_CHOICES, default='3')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_score_percent(self):
+        score_percent = round(self.score / 5 * 100)
+        return score_percent
+
+    class Meta:
+        unique_together = ('shop_id', 'user')
+
+    def __str__(self):
+        return str(self.shop_id)
+
